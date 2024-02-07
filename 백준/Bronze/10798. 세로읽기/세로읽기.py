f5944d2e42d96d3c7ffd9be5c274699034c4f516
gawona <104410699+gawona@users.noreply.github.com>
2024-02-07 17:35:38 +0900

    [Bronze I] Title: 세로읽기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,10 @@
+arr = [list(input()) for _ in range(5)]
+
+result = ''
+for i in range(15):
+    for j in range(5):
+        try:
+            result += arr[j][i]
+        except IndexError:
+            result += ''
+print(result)
