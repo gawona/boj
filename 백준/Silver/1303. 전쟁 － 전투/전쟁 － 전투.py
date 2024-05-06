c2fd720a84783655a38755768d0d0c2f9a2a961d
gawona <104410699+gawona@users.noreply.github.com>
2024-05-06 17:52:08 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 192 ms, Memory: 116216 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,42 @@
+from sys import stdin; input = stdin.readline
+di = [0, 0, 1, -1]
+dj = [1, -1, 0, 0]
+
+def dfs1(a, b):
+    global cnt
+    for k in range(4):
+        ni = a + di[k]
+        nj = b + dj[k]
+        if 0<=ni<M and 0<=nj<N and not visited[ni][nj] and arr[ni][nj] == 'W':
+            visited[ni][nj] = 1
+            cnt += 1
+            dfs1(ni, nj)
+
+def dfs2(a, b):
+    global cnt
+    for k in range(4):
+        ni = a + di[k]
+        nj = b + dj[k]
+        if 0<=ni<M and 0<=nj<N and not visited[ni][nj] and arr[ni][nj] == 'B':
+            visited[ni][nj] = 1
+            cnt += 1
+            dfs2(ni, nj)
+
+N, M = map(int, input().split())
+arr = [list(input()) for _ in range(M)]
+visited = [[0] * N for _ in range(M)]
+
+white = 0
+blue = 0
+for i in range(M):
+    for j in range(N):
+        cnt = 1
+        if arr[i][j] == 'W' and not visited[i][j]:
+            visited[i][j] = 1
+            dfs1(i, j)
+            white += cnt**2
+        elif arr[i][j] == 'B' and not visited[i][j]:
+            visited[i][j] = 1
+            dfs2(i, j)
+            blue += cnt**2
+print(white, blue)
