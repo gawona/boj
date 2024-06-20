1bd3c48d75db2d532915556c86a64ebb15e70b71
gawona <104410699+gawona@users.noreply.github.com>
2024-06-20 15:17:25 +0900

    [Silver III] Title: N과 M (7), Time: 1032 ms, Memory: 115728 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,17 @@
+def nnm():
+    if len(lst) == m:
+        print(' '.join(map(str, lst)))
+        return
+
+    for a in arr:
+        # if a not in lst:
+        lst.append(a)
+        nnm()
+        lst.pop()
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+lst = []
+
+nnm()
